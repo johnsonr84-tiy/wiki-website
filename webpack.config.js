5eccafc06f73e390782736ac19f9943971348ad4
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-04-04 16:45:30 -0600

    chore (update) Add installs in CL to update bundle.js
    Changed file path in package json

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 var path = require('path');
 
 module.exports = {
-  entry: ['./app/scripts/index.js', './app/styles/main.sass.js'],
+  entry: ['./app/scripts/index.js','./app/styles/main.sass'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'cheap-eval-source-map'
   watch: true,
   module: {
       rules: [{
